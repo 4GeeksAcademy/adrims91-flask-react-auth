a4b8a1b7344b37e103837b860eccbddb7c9dfeec
adrims91 <adrims91@gmail.com>
2025-01-11 09:57:37 +0000

    added changes in Login and Register divs

--- src/front/js/component/Login.js
+++ src/front/js/component/Login.js
@@ -22,41 +22,43 @@ export const Login = () => {
     return (
         <>
             {loading ? <Loading /> : <>
-                <h1 className="text-center">Acceder</h1>
-                <form
-                    className="text-center"
-                    onSubmit={e => {
-                        e.preventDefault();
-                        setLoading(true);
-                        login(email, password)
-                            .finally(() => {
-                                setLoading(false);
-                                setEmail("");
-                                setPassword("");
-                            });
-                    }}>
-                    <input
-                        className="form-control mb-2 text-center w-50 m-auto"
-                        type="text"
-                        value={email}
-                        onChange={e => setEmail(e.target.value)}
-                        placeholder="Email"
-                    />
-                    <input
-                        className="form-control mb-2 text-center w-50 m-auto"
-                        type="password"
-                        value={password}
-                        onChange={e => setPassword(e.target.value)}
-                        placeholder="Contraseña"
-                    />
-                    <input
-                        className="btn btn-success"
-                        value={"Acceder"}
-                        type="submit"
-                    />
-                    {state.message && <div><p className="text-success">{state.message}</p></div>}
-                    {state.error && <div><p className="text-danger">{state.error}</p></div>}
-                </form>
+                <div className="d-flex flex-column justify-content-center" style={{ height: '50vh' }}>
+                    <h1 className="text-center">Acceder</h1>
+                    <form
+                        className="text-center"
+                        onSubmit={e => {
+                            e.preventDefault();
+                            setLoading(true);
+                            login(email, password)
+                                .finally(() => {
+                                    setLoading(false);
+                                    setEmail("");
+                                    setPassword("");
+                                });
+                        }}>
+                        <input
+                            className="form-control mb-2 text-center w-50 m-auto"
+                            type="text"
+                            value={email}
+                            onChange={e => setEmail(e.target.value)}
+                            placeholder="Email"
+                        />
+                        <input
+                            className="form-control mb-2 text-center w-50 m-auto"
+                            type="password"
+                            value={password}
+                            onChange={e => setPassword(e.target.value)}
+                            placeholder="Contraseña"
+                        />
+                        <input
+                            className="btn btn-success"
+                            value={"Acceder"}
+                            type="submit"
+                        />
+                        {state.message && <div><p className="text-success">{state.message}</p></div>}
+                        {state.error && <div><p className="text-danger">{state.error}</p></div>}
+                    </form>
+                </div>
             </>
             }
         </>
--- src/front/js/component/Register.js
+++ src/front/js/component/Register.js
@@ -19,37 +19,39 @@ export const Register = () => {
 
     return (
         <>
-            <h1 className="text-center">Regístrate</h1>
-            <form
-                className="text-center"
-                onSubmit={e => {
-                    e.preventDefault();
-                    register(email, password);
-                    setEmail("");
-                    setPassword("");
-                }}>
-                <input
-                    className="form-control mb-2 text-center w-50 m-auto"
-                    type="text"
-                    value={email}
-                    onChange={e => setEmail(e.target.value)}
-                    placeholder="Email"
-                />
-                <input
-                    className="form-control mb-2 text-center w-50 m-auto"
-                    type="password"
-                    value={password}
-                    onChange={e => setPassword(e.target.value)}
-                    placeholder="Contraseña"
-                />
-                <input
-                    className="btn btn-success"
-                    value={"Crear usuario"}
-                    type="submit"
-                />
-                {state.message && <div><p className="text-success">{state.message}</p></div>}
-                {state.error && <div><p className="text-danger">{state.error}</p></div>}
-            </form>
+            <div className="d-flex flex-column justify-content-center" style={{ height: '50vh' }}>
+                <h1 className="text-center">Regístrate</h1>
+                <form
+                    className="text-center"
+                    onSubmit={e => {
+                        e.preventDefault();
+                        register(email, password);
+                        setEmail("");
+                        setPassword("");
+                    }}>
+                    <input
+                        className="form-control mb-2 text-center w-50 m-auto"
+                        type="text"
+                        value={email}
+                        onChange={e => setEmail(e.target.value)}
+                        placeholder="Email"
+                    />
+                    <input
+                        className="form-control mb-2 text-center w-50 m-auto"
+                        type="password"
+                        value={password}
+                        onChange={e => setPassword(e.target.value)}
+                        placeholder="Contraseña"
+                    />
+                    <input
+                        className="btn btn-success"
+                        value={"Crear usuario"}
+                        type="submit"
+                    />
+                    {state.message && <div><p className="text-success">{state.message}</p></div>}
+                    {state.error && <div><p className="text-danger">{state.error}</p></div>}
+                </form>
+            </div>
         </>
     )
 }
